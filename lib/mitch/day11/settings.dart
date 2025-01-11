4604eda00aaeb02c3eb77f10f8b7aac0d9cccf2f
Isaac Bihengoto <isaacbihengoto@gmail.com>
2025-01-11 17:21:53 +0300

    Drawer widget added a settings drawer

---
+++ lib/mitch/day11/settings.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class Settings extends StatelessWidget {
+  const Settings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("S E T T I N G S"),
+        backgroundColor: Colors.amber,
+      ),
+
+      body: Center(
+        child: Text("S E T T I N G S"),
+      )
+    );
+  }
+}
